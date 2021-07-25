3d69610b8a4fd4d1fb04e44deffb266c6e590118
michaelmurwayi <mikemurwayi7@gmail.com>
2021-07-25 15:27:05 +0300

    ensure issue items view is making stock deductions

--- stockmgmgt/views.py
+++ stockmgmgt/views.py
@@ -7,6 +7,7 @@ from .forms import StockCreateForm, StockSearchForm, StockUpdateForm, IssueForm,
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import FormView
 from .models import Transaction
+import datetime
 
 
 # Create your views here.
